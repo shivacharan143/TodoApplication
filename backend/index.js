79971513e032ff64098301b147c62bf41d2ca57f
Vadithe Shiva Charan <108936283+shivacharan143@users.noreply.github.com>
2025-06-08 11:45:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,11 +33,12 @@ try {
 }
 
 // routes
+
+app.use("/todo", todoRoute);
+app.use("/user", userRoute);
 app.get('/', (req, res) => {
   res.send('Server Started Sucessfully');
 });
-app.use("/todo", todoRoute);
-app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
